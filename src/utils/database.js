d3b61138732089719a94c747e70c1c6f7e8ab74f
ARDA <ar.dehghan.a@gmail.com>
2024-07-08 19:07:20 +0330

    refactor: Change database from mysql to postgres

--- src/utils/database.js
+++ src/utils/database.js
@@ -5,8 +5,9 @@ const sequelize = new Sequelize(
   process.env.DATABASE_USER,
   process.env.DATABASE_PASSWORD,
   {
-    dialect: 'mysql',
     host: 'localhost',
+    port: '5432',
+    dialect: 'postgres',
   }
 );
 
